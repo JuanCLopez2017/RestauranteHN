0d07d205827e8da44b670677cb05b49fb946f012
JuanCLopez2017 <juancarloslopezcarranza2017@gmail.com>
2022-08-14 20:58:29 -0600

    Agregando Entity FrameWork Code First

--- RestauranteHN/RestauranteHn.BL/ProductosBL.cs
+++ RestauranteHN/RestauranteHn.BL/ProductosBL.cs
@@ -8,29 +8,19 @@ namespace RestauranteHn.BL
 {
     public class ProductosBL
     {
+        Contexto _contexto;
+        public List<Producto> ListadeProductos { get; set; }
+
+        public ProductosBL()
+        {
+            _contexto = new Contexto();
+            ListadeProductos = new List<Producto>();
+        }
+
         public List<Producto> ObtenerProductos()
         {
-            var producto1 = new Producto();
-            producto1.Id = 1;
-            producto1.Descripcion = "Ensaladas";
-            producto1.Precio = 20;
-
-            var producto2 = new Producto();
-            producto2.Id = 2;
-            producto2.Descripcion = "Pastas";
-            producto2.Precio = 30;
-
-            var producto3 = new Producto();
-            producto3.Id = 3;
-            producto3.Descripcion = "Postres";
-            producto3.Precio = 35;
-
-            var listadeProductos = new List<Producto>();
-            listadeProductos.Add(producto1);
-            listadeProductos.Add(producto2);
-            listadeProductos.Add(producto3);
-
-            return listadeProductos; 
+            ListadeProductos= _contexto.Productos.ToList();
+            return ListadeProductos;
         }
 
     }
